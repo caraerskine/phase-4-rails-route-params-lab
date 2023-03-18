d79ecf80ef304ffce63e80342c7881627c8ba9ac
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-03-18 13:33:28 -0400

    commit tree ac9d2db1cfec01ebd03cd9e8b4ba2d7c502fea75

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,11 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    students = Student.find_by(id: params[:id])
+    render json: students
+    #render the values from the student record with the id
+    #find a student w the id
+  end
+
 end
